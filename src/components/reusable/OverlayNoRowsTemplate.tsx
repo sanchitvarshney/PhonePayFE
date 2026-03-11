39d9dd39b4e6afb99074eda0722f3a6d6804fd4f
sanchitvarshney <sanchit.varshney@mscorpres.in>
2026-03-11 17:54:43 +0530

    Add empty image

--- src/components/reusable/OverlayNoRowsTemplate.tsx
+++ src/components/reusable/OverlayNoRowsTemplate.tsx
@@ -1,7 +1,7 @@
 export const OverlayNoRowsTemplate = `
     <div>
       <div class="flex items-center justify-center w-full h-full no-rows-template">
-        <img src="/empty.png" class="w-[110px]" alt="No Data" />
+        <img src="/images/empty.png" class="w-[110px]" alt="No Data" />
       </div>
     </div>
     `;
