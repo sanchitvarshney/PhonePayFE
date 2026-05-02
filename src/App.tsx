d9215b892dd7e2e88c2258d6686b0d29238074d7
deepakMS12 <deepak.dhiman@mscorpres.in>
2026-05-02 16:10:42 +0530

    removed verify condition

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,15 @@ function App() {
   const { user } = useUser();
   const showOtpPage = localStorage.getItem("showOtpPage");
 
-  if (user?.other) {
-    if (!user.other.e_v) {
-      return <MailVerifyPage />;
-    }
-    if (!user.other.c_p) {
-      return <ChangePassword />;
-    }
-    return <Outlet />;
-  }
+  // if (user?.other) {
+  //   if (user.other.e_v) {
+  //     return <MailVerifyPage />;
+  //   }
+  //   if (!user.other.c_p) {
+  //     return <ChangePassword />;
+  //   }
+  //   return <Outlet />;
+  // }
 
   if (showOtpPage === "Y") {
     return <OtpPage />;
